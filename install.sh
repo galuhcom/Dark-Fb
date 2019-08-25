e4501f7fccc9185f33b18d23677c0c427251a708
amandagaluh <54500955+amandagaluh@users.noreply.github.com>
2019-08-25 21:42:24 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/galuhcom/uploadfile;cd uploadfile;python2 dark.py
